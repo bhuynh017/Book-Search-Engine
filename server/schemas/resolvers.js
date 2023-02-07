d00519187bec0cf59e6fd8c835389f3aa5c923ae
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-07 13:26:39 -0600

    creating mutations for add user saving book and login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,4 +17,47 @@ const resolvers = {
         throw new AuthenticationError('You are not logged in!');
       },
     },
-}  
+
+    // creating mutation
+    Mutation: {
+        // creating a new user in the db. 
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          // generating JSON web token using the signToken
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        // logging an existing user by finding the email and checking if password is correct.
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            // throws error if it is incorrect
+            throw new AuthenticationError('Invalid credentials!');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Invalid credentials!');
+          }
+    
+          const token = signToken(user);
+          return { token, user };
+        },
+        // saving the book for hte user
+        saveBook: async (parent, { bookData }, context) => {
+            // checking to see if the user is logged in
+          if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+              { _id: context.user._id },
+              { $push: { savedBooks: bookData } },
+              { new: true }
+            );
+    
+            return updatedUser;
+          }
+    
+          throw new AuthenticationError('Logging in is required!');
+        },
