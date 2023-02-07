cdbfe242f3f3e002bd46fb6a051ddeb9971fdc79
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-07 15:37:25 -0600

    creating a function that accepts the mongoid value

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -13,3 +13,34 @@ import { REMOVE_BOOK } from "../utils/mutations";
 import { removeBookId } from "../utils/localStorage";
 
 import Auth from "../utils/auth";
+
+// implementing SaveBooks 
+const SavedBooks = () => {
+    // Retrieving data about the currently logged in user and useQuery hook is used to fetch data
+    const { loading, data } = useQuery(QUERY_ME);
+    const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  
+    const userData = data?.me || {};
+    // executing a mutation to remove saved books from the users account
+    const handleDeleteBook = async (bookId) => {
+        // getting a token
+      const token = Auth.loggedIn() ? Auth.getToken() : null;
+  
+      if (!token) {
+        return false;
+      }
+  
+      try {
+        const { data } = await removeBook({
+          variables: { bookId },
+        });
+  
+        removeBookId(bookId);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+    if (loading) {
+      return <h2>LOADING...</h2>;
+    }
