b0c62af1f4f5f691be153d2b929d26b96204bb57
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-07 13:16:17 -0600

    adding hash password

--- server/models/User.js
+++ server/models/User.js
@@ -1,33 +1,48 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // importing schema from book.js
-const bookSchema = require('./Book');
+const bookSchema = require("./Book");
 
 // defining mongoose schema for userSchema.
 const userSchema = new Schema(
-    {
-      username: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Must use a valid email address'],
-      },
-      // adding bcrypt
-      password: {
-        type: String,
-        required: true,
-      },
-      savedBooks: [bookSchema],
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-      toJSON: {
-        virtuals: true,
-      },
-    }
-)
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    // adding bcrypt
+    password: {
+      type: String,
+      required: true,
+    },
+    savedBooks: [bookSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hashing the users password before saving it to the db.
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    // specifying the number of rounds
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
