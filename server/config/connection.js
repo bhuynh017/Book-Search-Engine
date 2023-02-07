cbede2765fa57c8f3f3952fa66facb55728f8182
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 20:49:48 -0600

    adding boiler plate code to connection and js files to models

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 // boiler plate code.
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearchengine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
