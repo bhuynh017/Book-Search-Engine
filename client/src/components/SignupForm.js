d1618f6b014c154ccc7c211d23cae9df4b7c3682
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-07 14:54:23 -0600

    adding signupform const

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -5,3 +5,26 @@ import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+
+const SignupForm = () => {
+    
+    const [userFormData, setUserFormData] = useState({
+      username: '',
+      email: '',
+      password: '',
+    });
+    
+    const [validated] = useState(false);
+    
+    const [showAlert, setShowAlert] = useState(false);
+  
+    const [addUser, { error }] = useMutation(ADD_USER);
+  
+    useEffect(() => {
+      if (error) {
+        setShowAlert(true);
+      } else {
+        setShowAlert(false);
+      }
+    }, [error]);
+}
