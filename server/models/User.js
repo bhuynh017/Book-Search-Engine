aeb174818f8b6599d8021c390b0bfd7c08d4ecf9
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 21:01:30 -0600

    adding the bookSchema

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+// importing schema constructor from mongoose
+const { Schema } = require("mongoose");
+
+// defining the mongoose schema for the collection
+// defining the structure and validation rules
+const bookSchema = new Schema({
+  authors: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  // saved book id from GoogleBooks
+  bookId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = bookSchema;
+
