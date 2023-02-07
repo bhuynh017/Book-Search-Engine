e81e628a87e1193fab90849f62f6fb345e426a1d
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 21:46:18 -0600

    adding userSchema

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,29 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+// importing schema from book.js
 const bookSchema = require('./Book');
+
+// defining mongoose schema for userSchema.
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address'],
+      },
+      // adding bcrypt
+      password: {
+        type: String,
+        required: true,
+      },
+   
+      savedBooks: [bookSchema],
+    },
+)
